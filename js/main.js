92adad7225a0a6bc9a16da12bbdb5716010d3188
Sem765887 <semyon.gardt765887@mail.ru>
2025-02-25 13:54:12 +0700

    перенос между колонок

--- js/main.js
+++ js/main.js
@@ -85,27 +85,22 @@ new Vue({
             this.saveData();
         },
         moveToPlanned(index) {
-            const task = this.tasks.splice(index, 1)[0];
-            this.plannedTasks.push(task);
-            this.saveData();
+
+        saveData() {
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+            localStorage.setItem('plannedTasks', JSON.stringify(this.plannedTasks));
+            localStorage.setItem('inProgressTasks', JSON.stringify(this.inProgressTasks));
+            localStorage.setItem('testingTasks', JSON.stringify(this.testingTasks));
+            localStorage.setItem('completedTasks', JSON.stringify(this.completedTasks));
         },
-        moveToInProgress(index) {
-            const task = this.plannedTasks.splice(index, 1)[0];
-            this.inProgressTasks.push(task);
-            this.saveData();
-        },
-        moveToTesting(index) {
-            const task = this.inProgressTasks.splice(index, 1)[0];
-            this.testingTasks.push(task);
-            this.saveData();
-        },
-        moveToCompleted(index) {
-            const task = this.testingTasks.splice(index, 1)[0];
-            task.status = 'Выполнено';
-            this.completedTasks.push(task);
-            this.saveData();
-        },
-        ,
+        loadData() {
+            this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+            this.plannedTasks = JSON.parse(localStorage.getItem('plannedTasks')) || [];
+            this.inProgressTasks = JSON.parse(localStorage.getItem('inProgressTasks')) || [];
+            this.testingTasks = JSON.parse(localStorage.getItem('testingTasks')) || [];
+            this.completedTasks = JSON.parse(localStorage.getItem('completedTasks')) || [];
+        }
+    },
     created() {
         this.loadData();
     }
